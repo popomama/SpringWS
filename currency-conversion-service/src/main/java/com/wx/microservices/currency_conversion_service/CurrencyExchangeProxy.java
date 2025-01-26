ee2c8e6771bdf87edd73ed5186c18b921691c98a
popomama <wxia2001@gmail.com>
2025-01-25 20:36:09 -0800

    unsuccessful conversion services

--- currency-conversion-service/src/main/java/com/wx/microservices/currency_conversion_service/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/wx/microservices/currency_conversion_service/CurrencyExchangeProxy.java
@@ -1,12 +1,18 @@
 package com.wx.microservices.currency_conversion_service;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //import com.wx.microservices.currency_exchange_service.CurrencyExchange;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@EnableFeignClients
+@FeignClient(name="currency-exchange-service") 
+//@FeignClient(name="currency-exchange-service", configuration=FeignClientsConfiguration.class) 
 public interface CurrencyExchangeProxy  {
 	
 
